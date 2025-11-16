2cf1e5ecb1a7f8832a09f3afa2fc5fe1e87a0a22
trymimi <applemimicom@gmail.com>
2025-11-17 04:54:21 +0900

    api, code 정의

---
+++ umc/src/main/java/com/example/umc/global/apiPayload/code/GeneralSuccessCode.java
@@ -0,0 +1,19 @@
+package com.example.umc.global.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GeneralSuccessCode implements BaseSuccessCode{
+
+    OK(HttpStatus.OK,
+            "COMMON200",
+            "성공입니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
