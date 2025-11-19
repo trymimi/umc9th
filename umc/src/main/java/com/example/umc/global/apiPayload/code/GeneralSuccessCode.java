13d690c08d68a0161b5576a15c49050ea78ceaeb
trymimi <applemimicom@gmail.com>
2025-11-19 17:52:54 +0900

    성공 응답 추가

--- umc/src/main/java/com/example/umc/global/apiPayload/code/GeneralSuccessCode.java
+++ umc/src/main/java/com/example/umc/global/apiPayload/code/GeneralSuccessCode.java
@@ -10,7 +10,16 @@ public enum GeneralSuccessCode implements BaseSuccessCode{
 
     OK(HttpStatus.OK,
             "COMMON200",
-            "성공입니다."),
+            "성공적으로 요청을 처리했습니다."),
+    CREATED(HttpStatus.CREATED,
+            "COMMON201",
+            "리소스가 성공적으로 생성되었습니다."),
+    ACCEPTED(HttpStatus.ACCEPTED,
+            "COMMON202",
+            "요청이 수락되었습니다."),
+    NO_CONTENT(HttpStatus.NO_CONTENT,
+            "COMMON204",
+            "요청이 성공적으로 처리되었습니다."),
     ;
 
     private final HttpStatus status;
